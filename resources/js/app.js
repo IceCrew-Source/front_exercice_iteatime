da9a246398948d576a6696b1e273e9f9a1d40123
IceCrew <loicviard12@gmail.com>
2024-05-08 03:19:06 +0200

    added responsive rendering

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+document.getElementById("dropdown_icon").addEventListener("click", showDropdownMenu);
+console.log(document.getElementsByClassName("menu_item").length)
+for (var i = 0; i < document.getElementsByClassName("menu_item").length; i++) {
+    document.getElementsByClassName("menu_item")[i].addEventListener("click", hideDropdownMenu);
+};
+
+function showDropdownMenu() {
+    document.getElementById("menu_items").style.transform="translateX(0)";
+    document.getElementById("menu_items").style.transition=" all 1s" 
+}
+
+function hideDropdownMenu() {
+    console.log("dd")
+    document.getElementById("menu_items").style.transform="translateX(-100%)";
+    document.getElementById("menu_items").style.transition=" all 1s" 
+}
